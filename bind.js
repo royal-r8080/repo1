a95a5b603c6afa20d978c9df3ca397858b975e51
rabiya <rabiyamulani2004@gmail.com>
2025-08-21 21:52:49 +0530

    Initial commit with test.html

---
+++ bind.js
@@ -0,0 +1,9 @@
+var app =angular.module("studentApp",[]);
+app.controller("StudentController" ,function ($scope) {
+    $scope.student1={
+        name:"John Doe",
+        roll:"101",
+        course:"Computer Science"
+
+    };
+});
